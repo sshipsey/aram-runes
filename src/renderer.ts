bebd73a35d40c2062e80d485ab990b319b54a346
Steve Shipsey <sashipsey@gmail.com>
2024-01-06 00:09:18 -0500

    sloppy POC working

--- src/renderer.ts
+++ src/renderer.ts
@@ -7,7 +7,10 @@ const el = (id: string) => document.getElementById(id);
 el('wv').addEventListener('dom-ready', () => {
   console.log('DOM READY!');
   let runes: { isActive: boolean; img: string }[] = [];
+  let shards: { isActive: boolean; img: string }[] = [];
+
   el('runes').innerHTML = '';
+  el('runes2').innerHTML = '';
   el('name').innerHTML = '';
   el('champImage').setAttribute('src', '');
   (el('wv') as WebviewTag)
@@ -22,25 +25,33 @@ el('wv').addEventListener('dom-ready', () => {
       el('champImage').setAttribute('src', $('.champion-image').attr('src'));
       el('name').innerHTML = $('.champion-name').html();
       el('loading').style.display = 'none';
-      console.log($('.perk').length);
       $('.perk').each((i, el) => {
         const isActive = $(el).hasClass('perk-active');
         const img = $(el).children()[0].attribs['src'];
         runes = runes.concat({ isActive, img });
       });
-      $('.perk').each((i, el) => {
-        const isActive = $(el).hasClass('perk-active');
+      $('.shard').each((i, el) => {
+        const isActive = $(el).hasClass('shard-active');
         const img = $(el).children()[0].attribs['src'];
-        runes = runes.concat({ isActive, img });
+        shards = shards.concat({ isActive, img });
       });
 
       runes = runes.slice(0, 22);
+      shards = shards.slice(0, 9);
+
       const runeEls = runes.map((rune) => {
         const img = document.createElement('img');
         img.src = rune.img;
         img.style.border = rune.isActive ? '4px solid green' : 'none';
         return img;
       });
+      const shardEls = shards.map((shard) => {
+        const img = document.createElement('img');
+        img.src = shard.img;
+        img.style.border = shard.isActive ? '4px solid green' : 'none';
+        return img;
+      });
+
       runeEls.forEach((runeEl, idx) => {
         if (idx < 13) {
           el('runes').appendChild(runeEl);
@@ -52,6 +63,12 @@ el('wv').addEventListener('dom-ready', () => {
           if (idx > 3 && idx % 3 === 0) {
             el('runes2').appendChild(document.createElement('br'));
           }
+        }
+      });
+      shardEls.forEach((shardEl, idx) => {
+        el('runes2').appendChild(shardEl);
+        if ((idx + 1) % 3 === 0) {
+          el('runes2').appendChild(document.createElement('br'));
         }
       });
     });
